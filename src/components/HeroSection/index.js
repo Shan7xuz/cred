7f1f80f3de13d3e5e874630f7b5058b31290ea12
Shan7xuz <96675411+Shan7xuz@users.noreply.github.com>
2022-03-21 12:51:01 +0530

    hero section responsive

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '../common/Button';
+import './heroSection.css';
+import '../common/Button/button.css';
+
+const HeroSection = () => {
+  return (
+    <div className='hero-section-wrapper'>
+      <div className='flex absolute-center hero-claim-label'>
+        <div className=''>pay credit card bill. earn guaranteed ₹200 back.</div>
+        <div className='claim-anchor'>
+          claim now
+          <img
+            src='https://web-images.credcdn.in/_next/assets/images/home-page/arrow.png'
+            alt=''
+            className='claim-arrow'
+          />
+        </div>
+      </div>
+      <div className='flex absolute-center flex-col hero-section max-width'>
+        <div className='hero-heading'>
+          rewards for paying credit card bills.
+        </div>
+        <div className='hero-subheading'>
+          join 7.5M+ members who win rewards and cashbacks everyday
+        </div>
+        <Button buttonText={'Download CRED'} />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
