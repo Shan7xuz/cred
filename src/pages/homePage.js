63cbad9c46eede01e2240a1816936ad91af246e9
Shan7xuz <96675411+Shan7xuz@users.noreply.github.com>
2022-03-23 11:17:32 +0530

    product section full responsive

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import Header from '../components/common/Header';
+import ProductShowcase from '../components/ProductShowcase';
+import FeelSpecial from '../components/FeelSpecial';
 
 const homePage = () => {
   return (
     <>
       <Header />
       <HeroSection />
+      <ProductShowcase />
+      <FeelSpecial />
     </>
   );
 };
