bc8e76658d3cda6da8c73534520efb2f681de8d4
Shan7xuz <96675411+Shan7xuz@users.noreply.github.com>
2022-03-24 09:33:52 +0530

    scroll section created

---
+++ src/components/MobileScroll/ScreenText.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ScreenText = ({ screen, setCurrentImg, i }) => {
+  return <div></div>;
+};
+
+export default ScreenText;
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import HeroSection from '../components/HeroSection';
 import Header from '../components/common/Header';
+import HeroSection from '../components/HeroSection';
 import ProductShowcase from '../components/ProductShowcase';
 import FeelSpecial from '../components/FeelSpecial';
-import CredExperience from '../components/CredExperience';
-import CredSecurity from '../components/CredSecurity';
 import BrandsLove from '../components/BrandsLove';
+import CredExperience from '../components/CredExperience';
+import MobileScroll from '../components/MobileScroll';
+import WindowPeak from '../components/WindowPeak';
+import CredSecurity from '../components/CredSecurity';
 
 const HomePage = () => {
   return (
@@ -16,6 +18,10 @@ const HomePage = () => {
       <FeelSpecial />
       <BrandsLove />
       <CredExperience />
+      <MobileScroll />
+      <div className='non-mobile'>
+        <WindowPeak />
+      </div>
       <CredSecurity />
     </>
   );
